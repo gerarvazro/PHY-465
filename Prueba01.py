05adb1db5079039baca4c6f224998b71eb7944b0
Gerardo Vazquez <pa@Eriks-MBP.lan>
2025-08-15 14:13:48 -0400

    Part00 Routines

---
+++ Prueba01.py
@@ -0,0 +1,13 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x = 3
+... y = 4
+... 
+... if (x == 4) and (y == 4):
+...     print(" First")
+...     
+... elif (x == 3) and (y == 4):
+...     print("Second")
+...     
+... else:
+...     print("Correct your statements")
